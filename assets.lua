6cbb60aaac7ded8ec1260e34ecb83e2e94dea532
Edvin Bruun <edvbru-9@student.ltu.se>
2012-11-20 20:28:01 +0100

    Managed a test for printing own background on first brick :D

--- assets.lua
+++ assets.lua
@@ -2,3 +2,8 @@
 
 IconAssets = group{quality=9.95}
 Icon = image{"icon.png"}
+
+Title = image{"title.png", width=144, height=64}
+
+GameAssets = group{}
+Background = image{"background.png", quality=10}
--- main.cpp
+++ main.cpp
@@ -4,17 +4,25 @@
 
 #include "game.h"
 
+static AssetSlot MainSlot = AssetSlot::allocate()
+    .bootstrap(GameAssets);
+	
 static Metadata M = Metadata()
     .title("ShiftingBricks")
-    .package("sb", "0.1")
+    .package("com.sifteo.shiftbrick", "0.1")
     .icon(Icon)
     .cubeRange(NUM_CUBES);
 
 void main() {
 
 	static Game game;
+	VideoBuffer vid;	
+	vid.initMode(BG0);
+	vid.attach(0);
+	vid.bg0.image(vec(0,0), Background);
 	
 	while(1){
+		System::paint();
 		/*
 		Run all Game functions
 		1. titlescreen / init
